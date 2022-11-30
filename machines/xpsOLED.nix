441541a9157755865d347d03f10a5db68fb3b28f
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2022-11-30 19:31:22 +0100

    Added xpsOLED configuration

---
+++ machines/xpsOLED.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, lib, ... }:
+
+let
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec "$@"
+  '';
+in
+{
+  config = {
+    boot.kernelPackages = pkgs.linuxPackages_latest;
+    languageservers.enable = true;
+    services.fwupd.enable = true;
+    services.fprintd.enable = true;
+
+    home-manager.users.jordy.dotfiles.isLaptop = true;
+    boot.loader = {
+      efi = {
+        canTouchEfiVariables = true;
+      };
+      grub = {
+        efiSupport = true;
+        device = "nodev";
+        useOSProber = true;
+      };
+    };
+
+    networking.hostName = "nixpsOLED";
+    networking.networkmanager.enable = true;
+    networking.wireguard.enable = true;
+    hardware.bluetooth.enable = true;
+    systemd.services.NetworkManager-wait-online.enable = false;
+
+    environment.systemPackages = with pkgs; [ nvidia-offload mesa-demos ];
+    services.xserver.videoDrivers = [ "nvidia" ];
+    hardware.nvidia.prime = {
+      offload.enable = true;
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+}
