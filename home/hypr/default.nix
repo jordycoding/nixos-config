def13f5bb2d7a91b888464f3d62ccc8b0734adc6
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2023-11-15 16:19:43 +0100

    More hyprland config

--- home/hypr/default.nix
+++ home/hypr/default.nix
@@ -1,13 +1,52 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+    ./colors.nix
+  ];
+
   wayland.windowManager.hyprland = {
     enable = true;
     extraConfig = ''
+      source=~/.config/hypr/mocha.conf
       $mod = SUPER
+      $left = h
+      $down = j
+      $right = l
+      $up = k
+
 
       bind = $mod, RETURN, exec, alacritty -e tmux
       bind = $mod SHIFT, q, killactive
+      bind = $mod SHIFT, e, exit
+
+      bind = $mod, $left, movefocus, l
+      bind = $mod, $down, movefocus, d
+      bind = $mod, $right, movefocus, r
+      bind = $mod, $up, movefocus, u
+
+      bind = $mod, d, exec, rofi -show drun
+
+      bind = $mod, r, submap, resize
+
+      bind = $mod SHIFT, $left, swapwindow, l
+      bind = $mod SHIFT, $down, swapwindow, d
+      bind = $mod SHIFT, $right, swapwindow, r
+      bind = $mod SHIFT, $up, swapwindow, u
+
+      bind = $mod SHIFT, D, exec, hyprctl keyword general:layout "dwindle"
+      bind = $mod SHIFT, M, exec, hyprctl keyword general:layout "master"
+
+      #resize
+      submap=resize
+      binde=,$right,resizeactive,10 0
+      binde=,$left,resizeactive,-10 0
+      binde=,$up,resizeactive,0 -10
+      binde=,$down,resizeactive,0 10
+
+      bind=,escape,submap,reset
+      submap = reset
+      
       #workspaces
       ${builtins.concatStringsSep "\n" (builtins.genList (
         x: let
@@ -30,6 +69,10 @@
 
       gestures {
         workspace_swipe = true
+      }
+
+      dwindle {
+        force_split = 2
       }
     '';
   };
