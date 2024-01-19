0f60c98c249c37168ce4690bcde28255f26da5d2
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2024-01-19 23:41:19 +0100

    Added icon theme to dconf

--- home/gnome/dconf.nix
+++ home/gnome/dconf.nix
@@ -8,9 +8,6 @@
     "org/gnome/desktop/peripherals/touchpad" = {
       "tap-to-click" = true;
     };
-    "org/gnome/desktop/interface" = {
-      "show-battery-percentage" = true;
-    };
     "org/gnome/desktop/wm/preferences" = {
       "button-layout" = "appmenu:minimize,maximize,close";
     };
@@ -18,8 +15,10 @@
       "disable-lock-screen" = false; # Why is this disabled exactly 
     };
     "org/gnome/desktop/interface" = {
+      "show-battery-percentage" = true;
       "gtk-theme" = "adw-gtk3-dark";
       "color-scheme" = "prefer-dark";
+      "icon-theme" = "Papirus-Dark";
     };
     "org/gnome/shell" = {
       "favorite-apps" = [ "firefox.desktop" "discord.desktop" "tidal-hifi.desktop" "1password.desktop" "com.raggesilver.BlackBox.desktop" "org.gnome.Nautilus.desktop" "org.gnome.Geary.desktop" "org.gnome.Calendar.desktop" "org.gnome.Settings.desktop" ];
