9b9cdb1a97e9be99c5d56ae85ecc3eeb4e8b797f
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2024-06-01 16:25:06 +0200

    Update nixos stable to 24.05

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
 
     # Official NixOS package source, using nixos-unstable branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     hyprland.url = "github:hyprwm/Hyprland";
