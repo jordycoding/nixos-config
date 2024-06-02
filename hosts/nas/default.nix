03abf722329607afb9e21a34daea14d1b3b5f822
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2024-06-02 20:06:33 +0200

    Nas local dns shit

--- hosts/nas/default.nix
+++ hosts/nas/default.nix
@@ -8,138 +8,145 @@
     ../../modules/server
     ./hardware-configuration.nix
   ];
-  age.secrets.wgPrivkey = {
-    file = ../../secrets/wgPrivkey.age;
-    mode = "770";
-    owner = "systemd-network";
-    group = "systemd-network";
-  };
-  age.secrets.ddPassword = {
-    file = ../../secrets/ddPassword.age;
+
+  options.services.unbound.settings.server.local-data = lib.mkOption {
+    type = lib.types.anything;
   };
 
-  networking.useNetworkd = true;
-  systemd.network.enable = true;
-  services.openssh.settings.PasswordAuthentication = false;
-  users.users.jordy = {
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH4s3aJHRoD5U8cTu1+NrvA1LTSDpAOqcTTF0p2L6UGF"
+  config = {
+    age.secrets.wgPrivkey = {
+      file = ../../secrets/wgPrivkey.age;
+      mode = "770";
+      owner = "systemd-network";
+      group = "systemd-network";
+    };
+    age.secrets.ddPassword = {
+      file = ../../secrets/ddPassword.age;
+    };
+
+    networking.useNetworkd = true;
+    systemd.network.enable = true;
+    services.openssh.settings.PasswordAuthentication = false;
+    users.users.jordy = {
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH4s3aJHRoD5U8cTu1+NrvA1LTSDpAOqcTTF0p2L6UGF"
+      ];
+    };
+
+    services.resolved = {
+      extraConfig = ''
+        DNSStubListener=no
+      '';
+    };
+    services.zfs.autoScrub.enable = true;
+
+    system.autoUpgrade = {
+      enable = true;
+      flake = inputs.self.outPath;
+      flags = [
+        "--update-input"
+        "nixpkgs"
+        "nixpkgs-stable"
+        "--print-build-logs"
+      ];
+      dates = "04:30";
+      randomizedDelaySec = "45min";
+    };
+
+    nixpkgs = {
+      overlays = [
+        outputs.overlays.unstable-packages
+        outputs.overlays.samba
+      ];
+    };
+
+    environment.systemPackages = with pkgs; [
+      intel-gpu-tools
+      inputs.nixguard.packages.x86_64-linux.default
+      wireguard-tools
     ];
-  };
 
-  services.resolved = {
-    extraConfig = ''
-      DNSStubListener=no
-    '';
-  };
-  services.zfs.autoScrub.enable = true;
+    environment.shellAliases = {
+      update = "(cd /etc/nixos; doas nix flake update) && doas nixos-rebuild switch --upgrade --flake /etc/nixos";
+    };
 
-  system.autoUpgrade = {
-    enable = true;
-    flake = inputs.self.outPath;
-    flags = [
-      "--update-input"
-      "nixpkgs"
-      "nixpkgs-stable"
-      "--print-build-logs"
+    hardware.opengl = {
+      enable = true;
+      extraPackages = with pkgs; [
+        intel-media-driver
+        intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+      ];
+    };
+    environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; };
+
+    users.users.jordy.extraGroups = [ "wheel" "libvirtd" "video" "media" "download" ];
+
+    users.users.testuser = {
+      isNormalUser = true;
+      shell = pkgs.zsh;
+    };
+
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.timeout = 0;
+    boot.loader.grub = {
+      enable = true;
+      zfsSupport = true;
+      efiSupport = true;
+      mirroredBoots = [
+        { devices = [ "nodev" ]; path = "/boot"; }
+      ];
+    };
+    boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+    boot.kernelParams = [ "i915.enable_guc=3" ];
+
+    networking.hostName = "Tungsten";
+    services.fstrim.enable = true;
+
+    networking.firewall = {
+      enable = true;
+      allowPing = true;
+    };
+
+    users.groups.media = { };
+    users.groups.download = { };
+
+    homelab.sonarr = true;
+    homelab.radarr = true;
+    homelab.prowlarr = true;
+    homelab.recyclarr = true;
+    homelab.sabnzbd = true;
+    homelab.samba = true;
+    homelab.plex = true;
+    homelab.caddy = true;
+    homelab.bazarr = true;
+    homelab.avahi = true;
+    homelab.dyndns = true;
+    homelab.gitea = true;
+    homelab.syncthing = true;
+    # homelab.grafana = true;
+    homelab.jellyfin = true;
+    homelab.openrgb = true;
+    homelab.unbound = {
+      enable = true;
+      blacklist = true;
+    };
+    # homelab.dnsmasq = {
+    #   enable = true;
+    #   blacklist = true;
+    # };
+
+    core.enableUI = false;
+
+    systemd.tmpfiles.rules = [
+      "d /mnt/Media/Series 0770 root media - -"
+      "d /mnt/Media/Movies 0770 root media - -"
+      "d /mnt/Media/Anime 0770 root media - -"
+      "d /mnt/Ssd/Downloads/Sab 0770 root download - -"
+      "d /mnt/Ssd/Downloads/Sab/incomplete 0770 root download - -"
+      "d /mnt/Ssd/Downloads/Sab/complete 0770 root download - -"
     ];
-    dates = "04:30";
-    randomizedDelaySec = "45min";
+
+    networking.hostId = "034146c2";
+    system.stateVersion = "23.11";
   };
-
-  nixpkgs = {
-    overlays = [
-      outputs.overlays.unstable-packages
-      outputs.overlays.samba
-    ];
-  };
-
-  environment.systemPackages = with pkgs; [
-    intel-gpu-tools
-    inputs.nixguard.packages.x86_64-linux.default
-    wireguard-tools
-  ];
-
-  environment.shellAliases = {
-    update = "(cd /etc/nixos; doas nix flake update) && doas nixos-rebuild switch --upgrade --flake /etc/nixos";
-  };
-
-  hardware.opengl = {
-    enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver
-      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
-    ];
-  };
-  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; };
-
-  users.users.jordy.extraGroups = [ "wheel" "libvirtd" "video" "media" "download" ];
-
-  users.users.testuser = {
-    isNormalUser = true;
-    shell = pkgs.zsh;
-  };
-
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.timeout = 0;
-  boot.loader.grub = {
-    enable = true;
-    zfsSupport = true;
-    efiSupport = true;
-    mirroredBoots = [
-      { devices = [ "nodev" ]; path = "/boot"; }
-    ];
-  };
-  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
-  boot.kernelParams = [ "i915.enable_guc=3" ];
-
-  networking.hostName = "Tungsten";
-  services.fstrim.enable = true;
-
-  networking.firewall = {
-    enable = true;
-    allowPing = true;
-  };
-
-  users.groups.media = { };
-  users.groups.download = { };
-
-  homelab.sonarr = true;
-  homelab.radarr = true;
-  homelab.prowlarr = true;
-  homelab.recyclarr = true;
-  homelab.sabnzbd = true;
-  homelab.samba = true;
-  homelab.plex = true;
-  homelab.caddy = true;
-  homelab.bazarr = true;
-  homelab.avahi = true;
-  homelab.dyndns = true;
-  homelab.gitea = true;
-  homelab.syncthing = true;
-  # homelab.grafana = true;
-  homelab.jellyfin = true;
-  homelab.openrgb = true;
-  homelab.unbound = {
-    enable = true;
-    blacklist = true;
-  };
-  # homelab.dnsmasq = {
-  #   enable = true;
-  #   blacklist = true;
-  # };
-
-  core.enableUI = false;
-
-  systemd.tmpfiles.rules = [
-    "d /mnt/Media/Series 0770 root media - -"
-    "d /mnt/Media/Movies 0770 root media - -"
-    "d /mnt/Media/Anime 0770 root media - -"
-    "d /mnt/Ssd/Downloads/Sab 0770 root download - -"
-    "d /mnt/Ssd/Downloads/Sab/incomplete 0770 root download - -"
-    "d /mnt/Ssd/Downloads/Sab/complete 0770 root download - -"
-  ];
-
-  networking.hostId = "034146c2";
-  system.stateVersion = "23.11";
 }
--- modules/server/unbound.nix
+++ modules/server/unbound.nix
@@ -13,41 +13,32 @@ lib.mkIf (config.homelab.unbound.enable)
       settings = {
         server = {
           interface = [ "::1" "127.0.0.1" "192.168.1.74" ];
+          access-control = "192.168.1.0/24 allow";
+          local-data = [
+            "\"tungsten.home.arpa A 192.168.1.74\""
+          ]
+          ++ optionals (config.homelab.caddy && config.homelab.sonarr) [
+            "\"sonarr.home.arpa A 192.168.1.74\""
+          ]
+          ++ optionals (config.homelab.caddy && config.homelab.radarr) [
+            "\"radarr.home.arpa A 192.168.1.74\""
+          ]
+          ++ optionals (config.homelab.caddy && config.homelab.prowlarr) [
+            "\"prowlarr.home.arpa A 192.168.1.74\""
+          ]
+          ++ optionals (config.homelab.caddy && config.homelab.sabnzbd) [
+            "\"sab.home.arpa A 192.168.1.74\""
+          ]
+          ++ optionals (config.homelab.caddy && config.homelab.bazarr) [
+            "\"bazarr.home.arpa A 192.168.1.74\""
+          ]
+          ++ optionals (config.homelab.caddy && config.homelab.grafana) [
+            "\"grafana.home.arpa A 192.168.1.74\""
+          ]
+          ++ optionals (config.homelab.caddy && config.homelab.grafana) [
+            "\"prometheus.home.arpa A 192.168.1.74\""
+          ];
         };
-        forward-zone = [
-          {
-            name = "tungsten.home.arpa";
-            forward-addr = [ "192.168.1.74" ];
-          }
-        ]
-        ++ optionals (config.homelab.caddy && config.homelab.sonarr) [{
-          name = "sonarr.home.arpa";
-          forward-addr = [ "192.168.1.74" ];
-        }]
-        ++ optionals (config.homelab.caddy && config.homelab.radarr) [{
-          name = "radarr.home.arpa";
-          forward-addr = [ "192.168.1.74" ];
-        }]
-        ++ optionals (config.homelab.caddy && config.homelab.prowlarr) [{
-          name = "prowlarr.home.arpa";
-          forward-addr = [ "192.168.1.74" ];
-        }]
-        ++ optionals (config.homelab.caddy && config.homelab.sabnzbd) [{
-          name = "sab.home.arpa";
-          forward-addr = [ "192.168.1.74" ];
-        }]
-        ++ optionals (config.homelab.caddy && config.homelab.bazarr) [{
-          name = "bazarr.home.arpa";
-          forward-addr = [ "192.168.1.74" ];
-        }]
-        ++ optionals (config.homelab.caddy && config.homelab.grafana) [{
-          name = "grafana.home.arpa";
-          forward-addr = [ "192.168.1.74" ];
-        }]
-        ++ optionals (config.homelab.caddy && config.homelab.grafana) [{
-          name = "prometheus.home.arpa";
-          forward-addr = [ "192.168.1.74" ];
-        }];
       };
     })
     (mkIf config.homelab.unbound.blacklist {
