7cfc966f4cf2dda74146eeeaa6af17cd7e774e43
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2024-06-13 22:13:44 +0200

    Keycloak proxy fix

--- modules/server/keycloak.nix
+++ modules/server/keycloak.nix
@@ -15,7 +15,7 @@ lib.mkIf (config.homelab.keycloak)
     settings = {
       hostname = "keycloak.alkema.co";
       http-port = 38080;
-      proxy = "passthrough";
+      proxy = "edge";
       http-enabled = true;
     };
   };
