084aa679c26e72025db2502b27537ce5ff5bde26
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2024-06-24 00:10:37 +0200

    Plasma 6

--- modules/wm/kde.nix
+++ modules/wm/kde.nix
@@ -3,7 +3,6 @@
 lib.mkIf (config.shell.kde)
 {
   services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-  services.xserver.displayManager.defaultSession = "plasmawayland";
+  # services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma6.enable = true;
 }
