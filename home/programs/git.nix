aeb90b49cea72f8122d15118db5b2467f8dc83e8
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2025-02-15 14:50:23 +0100

    Nixos home manager changes git

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   programs.git = {
@@ -6,13 +6,15 @@
     userName = "Jordy Alkema";
     userEmail = "6128820+jordycoding@users.noreply.github.com";
     lfs.enable = true;
+    signing = {
+      format = "ssh";
+      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINcC/4VsrWn7C0EgJE7wUfNtEeIaTM0Y/5eT4fl6+ZIO";
+      signByDefault = true;
+      signer = "${pkgs._1password-gui}/bin/op-ssh-sign";
+    };
     extraConfig = {
       pull.rebase = true;
       push.autoSetupRemote = true;
-      user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINcC/4VsrWn7C0EgJE7wUfNtEeIaTM0Y/5eT4fl6+ZIO";
-      gpg.format = "ssh";
-      gpg."ssh".program = "${pkgs._1password-gui}/bin/op-ssh-sign";
-      commit.gpgsign = true;
     };
   };
 }
